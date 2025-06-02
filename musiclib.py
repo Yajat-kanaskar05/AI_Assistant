d49e9c8c05e2bddafc97b10271fc092c1aaf15bf
Yajat-kanaskar05 <yajat.important@gmail.com>
2025-06-02 12:46:15 +0530

    Added a music library file

---
+++ musiclib.py
@@ -0,0 +1,8 @@
+music = {
+    "skyfall" : "https://youtu.be/DeumyOzKqgI" , 
+    "in the end" : "https://youtu.be/eVTXPUF4Oz4",
+    "numb" : "https://youtu.be/kXYiU_JCYtU",
+    "immortals" : "https://youtu.be/PHn5Q7hCjxw" , 
+    "final masquerade" : "https://youtu.be/1wF1sgXqF90",
+    "shape of you" : "https://youtu.be/JGwWNGJdvx8"
+}
